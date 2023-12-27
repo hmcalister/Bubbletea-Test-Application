4cbf10387f4e4f434e47deafbf3fcb0ddf7d20c7
Hayden McAlister <haydenmcalister49@gmail.com>
2023-12-27 19:04:11 +1300

    Add pagination logic to app

--- model/model.go
+++ model/model.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -15,21 +16,30 @@ type ApplicationStruct struct {
 	// Hold some state about the application
 
 	newItemTextInput textinput.Model
-	choices          []string
+	items            []string
 	cursor           int
 	selected         map[int]interface{}
+	pag              paginator.Model
 }
 
 // Create a new Application, initializing the values accordingly
 func NewApplicationStruct() ApplicationStruct {
 	newItemTextInput := textinput.New()
 	newItemTextInput.Placeholder = "Add New Item"
+	items := []string{"Item 1", "Item 2"}
+
+	pag := paginator.New()
+	pag.Type = paginator.Dots
+	pag.PerPage = 5
+	pag.ActiveDot = paginatorActiveDotStyle.Render("•")
+	pag.InactiveDot = paginatorInactiveDotStyle.Render("•")
 
 	return ApplicationStruct{
 		newItemTextInput: newItemTextInput,
-		choices:          []string{"Item 1", "Item 2"},
+		items:            items,
 		cursor:           0,
 		selected:         make(map[int]interface{}),
+		pag:              pag,
 	}
 }
 
@@ -55,6 +65,8 @@ func (app ApplicationStruct) Init() tea.Cmd {
 func (app ApplicationStruct) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
+	itemsOnPage := app.pag.ItemsOnPage(len(app.items))
+
 	// Check what kind of interaction has occurred
 	// Note we use a type switch so we can perform logic
 	// specific to each kind of message
@@ -79,19 +91,26 @@ func (app ApplicationStruct) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			app.cursor = max(app.cursor-1, 0)
 		case "down":
-			app.cursor = min(app.cursor+1, len(app.choices))
-			if app.cursor == len(app.choices) {
+			app.cursor = min(app.cursor+1, itemsOnPage)
+			if app.cursor == itemsOnPage {
 				return app, app.newItemTextInput.Focus()
+			}
+		case "left":
+			fallthrough
+		case "right":
+			if !app.newItemTextInput.Focused() {
+				app.cursor = 0
 			}
 
 		case " ":
 			if !app.newItemTextInput.Focused() {
 				// Check if the highlighted item is already selected or not
-				_, ok := app.selected[app.cursor]
+				itemIndex := app.cursor + app.pag.Page*app.pag.PerPage
+				_, ok := app.selected[itemIndex]
 				if ok {
-					delete(app.selected, app.cursor)
+					delete(app.selected, itemIndex)
 				} else {
-					app.selected[app.cursor] = struct{}{}
+					app.selected[itemIndex] = struct{}{}
 				}
 			}
 
@@ -103,9 +122,10 @@ func (app ApplicationStruct) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				}
 
 				// We have got a new item to add!
-				app.choices = append(app.choices, textInputValue)
+				app.items = append(app.items, textInputValue)
+				app.pag.SetTotalPages(len(app.items))
 				app.newItemTextInput.Reset()
-				app.cursor += 1
+				app.cursor = min(app.cursor+1, app.pag.PerPage)
 			} else {
 				// Check if the highlighted item is already selected or not
 				_, ok := app.selected[app.cursor]
@@ -120,9 +140,10 @@ func (app ApplicationStruct) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	if app.newItemTextInput.Focused() {
 		app.newItemTextInput, cmd = app.newItemTextInput.Update(msg)
+	} else {
+		app.pag, cmd = app.pag.Update(msg)
 	}
 
-	// Still not returning any commands.
 	return app, cmd
 }
 
@@ -133,25 +154,26 @@ func (app ApplicationStruct) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 // just printing a string out!
 func (app ApplicationStruct) View() string {
 	var appStringBuilder strings.Builder
-	appStringBuilder.WriteString(headerStyle.Render("TODO List Application") + "\n\n")
+	appStringBuilder.WriteString(headerStyle.Render("TODO List Application\nPage: "+app.pag.View()) + "\n")
 
 	// For each choice:
-	for index, choice := range app.choices {
+	currentPageItemsStart, currentPageItemsEnd := app.pag.GetSliceBounds(len(app.items))
+	for onPageIndex, choice := range app.items[currentPageItemsStart:currentPageItemsEnd] {
 		var currentItemStringBuilder strings.Builder
 		// If the cursor is on this choice, add it, but default to no cursor
 		cursor := "  "
-		if index == app.cursor {
+		if onPageIndex == app.cursor {
 			cursor = ">>"
 		}
 
 		// If the choice is selected, mark it so, but default to no mark
 		checked := " "
-		if _, ok := app.selected[index]; ok {
+		if _, ok := app.selected[onPageIndex+app.pag.Page*app.pag.PerPage]; ok {
 			checked = "x"
 		}
 
 		itemStr := fmt.Sprintf("%s [%s] %s", cursor, checked, choice)
-		if index == app.cursor {
+		if onPageIndex == app.cursor {
 			currentItemStringBuilder.WriteString(activeListItemStyle.Render(itemStr))
 		} else {
 			currentItemStringBuilder.WriteString(inactiveListItemStyle.Render(itemStr))
@@ -165,11 +187,5 @@ func (app ApplicationStruct) View() string {
 	// Add the text input to the bottom of the app
 	appStringBuilder.WriteString(textInputStyle.Render(app.newItemTextInput.View()))
 
-	// // Finally, add a footer
-	// if app.newItemTextInput.Focused() {
-	// 	appStringBuilder.WriteString("\n\n")
-	// } else {
-	// 	appStringBuilder.WriteString("\nPress q to quit.\n")
-	// }
 	return appStringBuilder.String()
 }
